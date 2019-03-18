5cd7dd94473c99c4421b83e508c7a3b33f3d9461
James Robertson <james@jessie.qbitx.com>
2019-03-18 08:56:34 +0000

    Working: This gem is now working at it was intended to. Replaced the RunEvery implementation with a simple while loop.

--- lib/humble_rpi-plugin-ld3w.rb
+++ lib/humble_rpi-plugin-ld3w.rb
@@ -2,33 +2,52 @@
 
 # file: humble_rpi-plugin-ld3w.rb
 
+# Intended for using a Nokia LD-3W with a Raspberry Pi from the Humble_RPi gem
 
 require 'ld3w'
-require 'run_every'
 
-
-# Intended for using a Nokia LD-3W with a Raspberry Pi from the Humble_RPi gem
 
 
 class HumbleRPiPluginLd3W
+  using ColouredText
 
-  def initialize(settings: {}, variables: {})
+  def initialize(settings: {}, variables: {}, debug: false)
 
     h = {rfdevice: 'rfcomm0', bdaddress: '', channel: '1', 
-         refresh_rate: 60}.merge settings
-    device_id = variables[:device_id] || 'pi'
+         refresh_rate: 120}.merge settings
+    @device_id = variables[:device_id] || 'pi'
+    @debug, @h, @notifier = debug, h, variables[:notifier]
 
-    ld3w = Ld3w.new device: h[:rfdevice], bdaddress: h[:bdaddress], 
-        channel: h[:channel]
-
-    RunEvery.new(seconds: h[:refresh_rate]) do
-
-      loc = ld3w.locate
-      variables[:notifier].notice "%s/gps: lat: %s lon: %s" % 
-                                      [device_id,  loc.latitude, loc.longitude]
+    @ld3w = Ld3w.new device: h[:rfdevice], bdaddress: h[:bdaddress], 
+        channel: h[:channel], verbose: false, affirmations: 5
+    
+  end
+  
+  def start()
+          
+    while true do
+        
+      begin
+        
+        loc = @ld3w.locate        
+        puts ('loc: ' + loc.inspect).debug if @debug
+        located = loc.is_a?(Array) ? loc.last : loc
+        @notifier.notice "%s/gps: lat: %s lon: %s" % 
+                              [@device_id,  located.latitude, located.longitude]
+      
+      rescue
+        
+        @notifier.notice "%s/gps/warning: %s" % [@device_id, 
+                                                 'unable to connect']
+        `service bluetooth restart`
+        sleep 6
+        retry
+        
+      end 
+      
+      sleep(@h[:refresh_rate])                              
     end
     
-
   end
   
   alias on_start start  
